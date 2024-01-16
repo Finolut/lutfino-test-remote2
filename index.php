b113233bcc5eb02f8490e25da8f08375a80bedc8
Lutfino Athallah Pratama <finolut@gmail.com>
2024-01-16 21:38:10 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
     <h1>ambasing</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, cumque asperiores! Voluptas, maxime perferendis nam cumque tempora quisquam facilis impedit, alias repellat, praesentium temporibus? Dolore repellat iste esse obcaecati pariatur.</p>
 </body>
 </html>
